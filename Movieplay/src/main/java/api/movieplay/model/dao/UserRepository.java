e6df4c6f7fa0218e0be821bcc229dde8aefdc9c2
Commondevelopernp <56087940+commondevelopernp@users.noreply.github.com>
2024-06-13 15:43:41 -0300

    Update UserRepository.java

--- Movieplay/src/main/java/api/movieplay/model/dao/UserRepository.java
+++ Movieplay/src/main/java/api/movieplay/model/dao/UserRepository.java
@@ -8,8 +8,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-	Optional<User>  findByEmail(String email);
+	Optional<User> findByEmail(String email);
 	Optional<User> findById(Long id);
+	Optional<User> save(User user);
 	void deleteById(Long id);
 	
 }
