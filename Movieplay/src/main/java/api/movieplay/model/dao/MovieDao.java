857c8ae72dcfab5de3be295d2ad5a5e03fb65194
Commondevelopernp <56087940+commondevelopernp@users.noreply.github.com>
2024-07-18 11:00:22 -0300

    Update MovieDao.java

--- Movieplay/src/main/java/api/movieplay/model/dao/MovieDao.java
+++ Movieplay/src/main/java/api/movieplay/model/dao/MovieDao.java
@@ -8,7 +8,7 @@ import api.movieplay.model.entity.Movie;
 
 public interface MovieDao {
     List<Movie> getAllMovies(String genre, String title, String actor, String sort, String order, Integer rate,int page, int pageSize);
-    Movie getMovieById(String id);
+    Movie getMovieById(Long id);
 	Movie save(Movie movie);
 
 
