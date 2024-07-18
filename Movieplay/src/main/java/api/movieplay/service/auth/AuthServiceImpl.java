59d9eaf35143535ec7c6911c3c5be74b1f3e4f09
Commondevelopernp <56087940+commondevelopernp@users.noreply.github.com>
2024-07-18 11:15:05 -0300

    Update AuthServiceImpl.java

--- Movieplay/src/main/java/api/movieplay/service/auth/AuthServiceImpl.java
+++ Movieplay/src/main/java/api/movieplay/service/auth/AuthServiceImpl.java
@@ -129,6 +129,10 @@ public class AuthServiceImpl implements AuthService {
         claims.put("email", user.getEmail());
         claims.put("firstName", user.getFirstName());
         claims.put("lastName", user.getLastName());
+        claims.put("id",user.getId());
+        claims.put("refresh",user.getRefresh());
+        claims.put("ratings",user.getRatings());
+        claims.put("favorited",user.getFavorited());
 
         return Jwts.builder()
                 .setClaims(claims)
