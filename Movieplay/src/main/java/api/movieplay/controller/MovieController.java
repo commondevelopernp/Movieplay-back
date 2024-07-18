24a023bb0835a07fcadabb88667742ef3e9be38b
Commondevelopernp <56087940+commondevelopernp@users.noreply.github.com>
2024-07-18 10:26:15 -0300

    Update MovieController.java

--- Movieplay/src/main/java/api/movieplay/controller/MovieController.java
+++ Movieplay/src/main/java/api/movieplay/controller/MovieController.java
@@ -1,13 +1,12 @@
 package api.movieplay.controller;
 
-
 import api.movieplay.model.entity.Movie;
 import api.movieplay.service.movie.MovieServiceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/movies")
@@ -25,19 +24,23 @@ public class MovieController {
                                     @RequestParam(required = false, defaultValue = "asc") String order,
                                     @RequestParam(required = false, defaultValue = "1") int page,
                                     @RequestParam(required = false, defaultValue = "10") int pageSize) {
-        return movieService.getAllMovies(genre, title, actor, sort, order,rate, page, pageSize);
+        return movieService.getAllMovies(genre, title, actor, sort, order, rate, page, pageSize);
     }
 
     @PostMapping
     public Movie createMovie(@RequestBody Movie movie) {
         return movieService.saveMovie(movie);
     }
+
     @GetMapping("/{id}")
     public Movie getMovieById(@PathVariable String id) {
-        Movie movie = movieService.getMovieById(id);
-        return movie;
+        return movieService.getMovieById(id);
     }
 
-
-
+    @PutMapping("/{id}")
+    public Movie updateMovie(@PathVariable String id, @RequestBody Movie movie) {
+        movie.setId(id); // Asegúrate de que el ID de la película sea el mismo que el del path variable
+        return movieService.saveMovie(movie);
+    }
 }
+
