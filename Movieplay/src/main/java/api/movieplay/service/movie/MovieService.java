b7030e8daa82c1046c629d7ab76dc0d915c44b3f
Commondevelopernp <56087940+commondevelopernp@users.noreply.github.com>
2024-07-18 15:38:37 -0300

    Update MovieService.java

--- Movieplay/src/main/java/api/movieplay/service/movie/MovieService.java
+++ Movieplay/src/main/java/api/movieplay/service/movie/MovieService.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface MovieService {
     List<Movie> getAllMovies(String genre, String title, String actor, String sort, String order, Integer rate, int page, int pageSize);
     Movie getMovieById(Long id);
+     List<Movie> getMoviesByIds(List<Long> ids);
     Movie saveMovie(Movie movie);
 
 }
